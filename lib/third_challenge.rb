45188218ee73bb95e5c5c75e05ab2b65ad283e9d
Daniel Reina <dannyflatiron@gmail.com>
2019-11-13 23:24:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {:name => "", :age => ""
+      :patriarch => {:name => "Lord Capulet", :age => "50"
         #your key/value pairs here
         },
       :matriarch => {:name => "Lady Capulet", :age => "51"
