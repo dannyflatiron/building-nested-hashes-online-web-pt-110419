f6297683bbac8b6f0c5ef5ecf816a714e58924e1
Daniel Reina <dannyflatiron@gmail.com>
2019-11-13 23:44:41 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,7 +1,7 @@
 def fourth_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
+      :patriarch => {:name => "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
@@ -14,6 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {:age => "30", :attitude => "confused", :name => "Steven"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
